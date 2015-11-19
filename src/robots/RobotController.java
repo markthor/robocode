9757177c43477f70c5e24017c04f73db37065484
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2015-11-19 14:47:44 +0100

    Added method to load activator

--- src/robots/RobotController.java
+++ src/robots/RobotController.java
@@ -1,19 +1,33 @@
 package robots;
 
+import org.jgap.Chromosome;
+
 import robocode.AdvancedRobot;
 
 import com.anji.integration.Activator;
+import com.anji.integration.ActivatorTranscriber;
+import com.anji.integration.TranscriberException;
+import com.anji.persistence.FilePersistence;
+import com.anji.persistence.Persistence;
+import com.anji.util.DummyConfiguration;
 
 public class RobotController extends AdvancedRobot {
+	private static ActivatorTranscriber activatorFactory = new ActivatorTranscriber();
+	private static Persistence db = new FilePersistence();
 	private RobotActor actor;
 	private RobotSensor sensor;
 	private Activator activator;
 	
-	public RobotController(RobotActor actor, RobotSensor sensor, String activatorFilePath) {
+	public RobotController(RobotActor actor, RobotSensor sensor, Long chromosomeId) {
 		super();
 		this.actor = actor;
 		this.sensor = sensor;
-		activator = loadActivator(activatorFilePath);
+		try {
+			activator = loadActivator(chromosomeId);
+		} catch(TranscriberException e) {
+			//TODO: Handle error
+			e.printStackTrace();
+		}
 	}
 	
 	public void run() {
@@ -22,7 +36,8 @@ public class RobotController extends AdvancedRobot {
 		}
 	}
 	
-	private Activator loadActivator(String activatorFilePath) {
-		return null;
+	private Activator loadActivator(Long chromosomeId) throws TranscriberException {
+		Chromosome chromosome = db.loadChromosome(chromosomeId.toString(), new DummyConfiguration());
+		return activatorFactory.newActivator(chromosome);
 	}
 }
