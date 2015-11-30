1ede7ab52bb5b11e8ed5880610dc5f355ec4fb2d
Mark <markthorh@gmail.com>
2015-11-30 11:39:02 +0100

    fixed the body

--- src/pacman/controller/PacmanController.java
+++ src/pacman/controller/PacmanController.java
@@ -14,6 +14,7 @@ import pacman.game.Game;
 public class PacmanController extends Controller<MOVE> {
 
 	private Activator neuralNetwork; 
+	protected static final double MAX_DISTANCE = 255.0;
 	
 	@Override
 	public MOVE getMove(Game game, long timeDue) {
@@ -30,8 +31,8 @@ public class PacmanController extends Controller<MOVE> {
 		int highestNeighbour = -1;
 		double highestNeighbourScore = -1.0;
 		for(int i = 0; i < neighbours.length; i++) {
-			double evaluation = evaluateNode(neighbours[i], game);
-			if(evaluateNode(neighbours[i], game) > highestNeighbourScore) {
+			double evaluation = evaluateNode(neighbours[i], game)[0];
+			if(evaluation > highestNeighbourScore) {
 				highestNeighbourScore = evaluation;
 				highestNeighbour = neighbours[i];
 			}
@@ -48,26 +49,60 @@ public class PacmanController extends Controller<MOVE> {
 	/**
 	 * Returns the output of the neural network based on the inputs of the provided node.
 	 */
-	protected double evaluateNode(int node, Game game) {
+	protected double[] evaluateNode(int node, Game game) {
 		return neuralNetwork.next(getInputFromGameStateAndNode(node, game));
 	}
 	
 	/**
 	 * Calculates the inputs to the network and scale them.
 	 */
-	@Override
-	protected List<Double> getInputFromGameStateAndNode(int node, Game game) { 
-		List<Double> input = new ArrayList<Double>();
-		input.add(scaleDistance(getDistanceToGhostFromNode(GHOST.BLINKY, game, node)));
-		input.add(scaleDistance(getDistanceToGhostFromNode(GHOST.INKY, game, node)));
-		input.add(scaleDistance(getDistanceToGhostFromNode(GHOST.PINKY, game, node)));
-		input.add(scaleDistance(getDistanceToGhostFromNode(GHOST.SUE, game, node)));
-		input.add(isGhostEdible(GHOST.BLINKY, game));
-		input.add(isGhostEdible(GHOST.INKY, game));
-		input.add(isGhostEdible(GHOST.PINKY, game));
-		input.add(isGhostEdible(GHOST.SUE, game));
-		input.add(scaleDistance(getDistanceToNearestPowerPill(node, game)));
-		input.add(scaleDistance(getDistanceToNearestPill(node, game)));
-		return input;
+	protected double[] getInputFromGameStateAndNode(int node, Game game) { 
+		double[] result = new double[10];
+		result[0] = scaleDistance(getDistanceToGhostFromNode(GHOST.BLINKY, game, node));
+		result[1] = scaleDistance(getDistanceToGhostFromNode(GHOST.INKY, game, node));
+		result[2] = scaleDistance(getDistanceToGhostFromNode(GHOST.PINKY, game, node));
+		result[3] = scaleDistance(getDistanceToGhostFromNode(GHOST.SUE, game, node));
+		result[4] = isGhostEdible(GHOST.BLINKY, game);
+		result[5] = isGhostEdible(GHOST.INKY, game);
+		result[6] = isGhostEdible(GHOST.PINKY, game);
+		result[7] = isGhostEdible(GHOST.SUE, game);
+		result[8] = scaleDistance(getDistanceToNearestPowerPill(node, game));
+		result[9] = scaleDistance(getDistanceToNearestPill(node, game));
+		return result;
+	}
+	
+	protected double isGhostEdible(GHOST ghost, Game game) {
+		if(game.isGhostEdible(ghost)) {
+			return 1.0;
+		} else {
+			return 0.0;
+		}
+	}
+	
+	/**
+	 * Scales the distance between 0 and 1.
+	 */
+	protected double scaleDistance(double distance) {
+		return distance/MAX_DISTANCE;
+	}
+	
+	protected double getDistanceToNearestPowerPill(int node, Game game) {
+		int[] powerPills = game.getActivePowerPillsIndices();
+		if(powerPills.length == 0) {
+			return 1.0;
+		}
+		return game.getShortestPathDistance(node, game.getClosestNodeIndexFromNodeIndex(node, powerPills, DM.PATH));
+	}
+	
+	protected double getDistanceToNearestPill(int node, Game game) {
+		int[] pills = game.getActivePillsIndices();
+		if(pills.length == 0) {
+			return 1.0;
+		}
+		return game.getShortestPathDistance(node, game.getClosestNodeIndexFromNodeIndex(node, pills, DM.PATH));
+	}
+	
+	protected double getDistanceToGhostFromNode(GHOST ghost, Game game, int node) {
+		return game.getShortestPathDistance(node, game.getGhostCurrentNodeIndex(ghost));
 	}
 }
